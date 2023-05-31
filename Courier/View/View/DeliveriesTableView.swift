8c503c67d267c29731a3e7161c4dc895e22ac341
hasanbayliveysal <hasanbayliveysaldev@gmail.com>
2023-05-31 12:01:13 +0400

    deliveries_tableview

---
+++ Courier/View/View/DeliveriesTableView.swift
@@ -0,0 +1,20 @@
+//
+//  DeliveriesTableView.swift
+//  Courier
+//
+//  Created by Veysal on 31.05.2023.
+//
+
+import UIKit
+
+class DeliveriesTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
